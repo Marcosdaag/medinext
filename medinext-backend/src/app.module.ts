dba28cbea0a5afb294a08944a6b860a021b177b1
Marcosdaag <marcosoffs99@gmail.com>
2026-02-10 15:51:30 -0300

    new nestproject

--- medinext-backend/src/app.module.ts
+++ medinext-backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
